07f293cbee45b38d50d78179d1cd2b2e65db7b98
DinSchlimmsteAlptraum <flobo1991@gmail.com>
2019-06-22 09:10:00 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import muuvyLogo from './img/logo.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class LoginPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      error: '',
+    };
+
+    this.handleUserChange = this.handleUserChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.dismissError = this.dismissError.bind(this);
+  }
+
+  dismissError() {
+    this.setState({ error: '' });
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+
+    if (!this.state.username) {
+      return this.setState({ error: 'Username is required' });
+    }
+
+    return this.setState({ error: '' });
+  }
+
+  handleUserChange(evt) {
+    this.setState({
+      username: evt.target.value,
+    });
+  };
+
+  render() {
+ 
+    return (
+    <div className="App-Wrapper">
+        <div className="App-Login">
+            <div class="LogoArea">
+              <div class="LogoCenter">
+              <img title="muuvy logo" class="Header__Logo" src={muuvyLogo} alt="" />
+              <h1 class="Header__Title">muuvy</h1>
+              </div>
+            </div>
+                    
+            
+
+          <div class="LoginForm">
+          <form onSubmit={this.handleSubmit}>
+              {
+                this.state.error &&
+                <h3 data-test="error" onClick={this.dismissError}>
+                  <button onClick={this.dismissError}>✖</button>
+                  {this.state.error}
+                </h3>
+              }
+              <h2 class="signInLabel">Sign in to muuvy</h2>
+              <label>Username</label>
+              <input type="text" data-test="username" value={this.state.username} onChange={this.handleUserChange}/> 
+            <input id="submit" type="submit" value="Login" data-test="submit" />  
+           
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default LoginPage;
---
+++ src/loginScreen/loginScreen.js
@@ -0,0 +1,54 @@
+//import React, { Component } from "react";
+//import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import "./loginScreen.css";
+
+export default class loginScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+    };
+  }
+
+  validateForm() {
+    return this.state.username.length > 0;
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="loginScreen">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="username" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+              autoFocus
+              type="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+        
+          <Button
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >
+            Login
+          </Button>
+        </form>
+      </div>
+    );
+  }
+}
